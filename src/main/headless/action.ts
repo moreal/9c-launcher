ea2ffdf4e2e45786056a1c7ee37080d6ff0d6ac5
moreal <dev.moreal@gmail.com>
2022-11-24 20:04:27 +0900

    Hardcode minter

--- src/main/headless/action.ts
+++ src/main/headless/action.ts
@@ -1,4 +1,6 @@
 import { StandaloneSubcommand } from "./subcommand";
+import { encode } from "bencodex";
+import { writeFileSync } from "fs";
 
 export class Action extends StandaloneSubcommand {
   public ActivateAccount(
@@ -51,15 +53,26 @@ export class Action extends StandaloneSubcommand {
     path: string
   ): boolean {
     try {
-      this.execSync(
-        "action",
-        "transfer-asset",
-        sender,
-        recipient,
-        `${amount}`,
-        path,
-        memo
-      );
+      const encoded = encode({
+        type_id: "transfer_asset3",
+        values: {
+          recipient: recipient,
+          sender: sender,
+          amount: [
+            {
+              decimalPlaces: Buffer.from([0x02]),
+              minters: [
+                Buffer.from("2c2a05e29e8f57c4661fb8fff5e0c7a7e0f3c4fc", "hex"),
+              ],
+              ticker: "NCG",
+            },
+            amount,
+          ],
+          ...(memo == null ? {} : { memo: memo }),
+        },
+      });
+
+      writeFileSync(path, encoded);
       return true;
     } catch (error) {
       console.error(error);
